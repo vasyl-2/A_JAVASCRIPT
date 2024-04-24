d8c22aefbb1c513d32fad3777671d5d7460da922
Vasiliy <dvv03122014@gmail.com>
2024-04-25 01:20:52 +0200

    worker

--- js/cache/calc.js
+++ js/cache/calc.js
@@ -1,3 +1,15 @@
+/*
+* Here, the runCalculation function is synchronous when the result has already
+been cached but is asynchronous otherwise. There are three possible
+scenarios.
+• The formula has already been computed, so the result is in the calculationCache. In this case, runCalculation is synchronous.
+• The formula has been sent to the worker, but the result hasn’t been
+received yet. In this case, runCalculation sets a timeout to call itself again;
+the process will repeat until the result is in calculationCache.
+• The formula hasn’t yet been sent to the worker. In this case, we’ll invoke
+the callback from the worker’s 'message' event listener*/
+
+
 const calculationCache = {},
     calculationCallbacks = {},
     mathWorker = new Worker('js/cache/worker.js');
@@ -11,10 +23,13 @@ mathWorker.addEventListener('message', function(e) {
 
 function runCalculation(formula, callback) {
     if (formula in calculationCache) {
-        return callback(calculationCache[formula]);
+        // return callback(calculationCache[formula]);
+        callback(calculationCache[formula]);
+        return;
     }
 
     if (formula in calculationCallbacks) {
+        console.log('HERE')
         return setTimeout(function() {
             runCalculation(formula, callback);
         }, 0);
